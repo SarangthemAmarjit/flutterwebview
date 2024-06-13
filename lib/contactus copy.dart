b84dcf5443a7c144a22caf55f6f2385f233776e9
Sarangthem Amarjit Meetei <sarangthemamarjit123@gmail.com>
2024-06-13 03:33:29 -0700

    Updated

--- lib/contactus copy.dart
+++ lib/contactus copy.dart
@@ -1,5 +1,7 @@
+import 'dart:convert';
 import 'dart:developer';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_inappwebview/flutter_inappwebview.dart';
 import 'package:flutterwebview/homepage.dart';
 
@@ -24,6 +26,7 @@ class _IlpPageState extends State<IlpPage> {
     return Scaffold(
       backgroundColor: const Color.fromARGB(255, 201, 200, 200),
       body: SafeArea(
+        minimum: const EdgeInsets.symmetric(vertical: 0),
         child: Stack(
           children: [
             InAppWebView(
@@ -42,6 +45,8 @@ class _IlpPageState extends State<IlpPage> {
                 webViewController = controller;
               },
               onLoadStart: (controller, url) {
+                // Load the font file as bytes
+
                 setState(() {
                   isLoading = true; // Show progress indicator
                 });
@@ -49,22 +54,50 @@ class _IlpPageState extends State<IlpPage> {
               },
               onLoadStop: (controller, url) async {
                 print('Finished loading: $url');
+                ByteData fontData =
+                    await rootBundle.load('assets/fonts/KulimPark-Regular.ttf');
+                Uint8List fontBytes = fontData.buffer.asUint8List();
+                String base64Font = base64.encode(fontBytes);
                 await controller.injectCSSCode(source: '''
-          body {
-    font-family: "Roboto,sans-serif;
-    font-size: 16px;
-    line-height: 1.42857143;
-    color: #333;
-    background-color: #fff;
-}
-          
+
+                          @font-face {
+                              font-family: 'KulimPark-Regular';
+                              src: url(data:font/ttf;base64,$base64Font) format('truetype');
+                            }
+                                                                      .container {
+                                                  font-family: "KulimPark-Regular"; 
+                                                  font-size: 14px; 
+                                            
+                                              }
+                                              .h1, h1 {
+                                display: none;
+                        }
+                        .h2, h2 {
+                            padding-bottom: 30px;
+                            font-weight: bold; 
+                              margin-top: -20px;
+  
+                        }
+
+
+                                                  td, th {
+                              padding-right: 7px;
+                          }
+
+
+
+                                .navbar-fixed-top {
+                                    display: none;   
+                                }
+                                                .navbar-default {
+                          display: none;
+                      }
+                                          
+                                                                                        
           ''');
                 // Inject JavaScript to remove the green footer and add padding at the end of the content
                 await controller.evaluateJavascript(source: """
                   document.getElementById('footer').style.display='none';
-                  document.getElementsByClassName('navbar-wrapper')[0].style.display = 'none';
-
- 
 
                   var paddingDiv = document.createElement('div');
                   paddingDiv.style.height = '50px'; // Adjust the height as needed
